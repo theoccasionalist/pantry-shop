1d6aa847bea7f5038ece813f74df235672251463
theoccasionalist <mrmccall07@gmail.com>
2019-07-23 20:12:45 -0400

    removed comments from navbar ts file

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,39 +13,22 @@ export class NavbarComponent implements OnInit {
 
   private auth0Client: Auth0Client;
 
-  /**
-   * Constructor - inject the AuthService class
-   */
   constructor(private authService: AuthService) {}
 
-  /**
-   * Handle component initialization
-   */
   async ngOnInit() {
-    // Get an instance of the Auth0 client
     this.auth0Client = await this.authService.getAuth0Client();
-
-    // Watch for changes to the isAuthenticated state
     this.authService.isAuthenticated.subscribe(value => {
       this.isAuthenticated = value;
     });
-
-    // Watch for changes to the profile data
     this.authService.profile.subscribe(profile => {
       this.profile = profile;
     });
   }
 
-  /**
-   * Logs in the user by redirecting to Auth0 for authentication
-   */
   async login() {
     await this.auth0Client.loginWithRedirect({});
   }
 
-  /**
-   * Logs the user out of the applicaion, as well as on Auth0
-   */
   logout() {
     this.auth0Client.logout({
       client_id: this.authService.config.client_id,
